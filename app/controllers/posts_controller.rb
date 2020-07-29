b2e7f97a2115af8d47f4bda9fde26194aa9eea0d
nabilhayet <nabil4457@yahoo.com>
2020-07-29 17:44:06 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -22,6 +22,10 @@ class PostsController < ApplicationController
 		@post.update(post_params(:title))
 		redirect_to post_path(@post)
 	  end
+
+	  def edit
+		@post = Post.find(params[:id])
+	  end
 	   
 	  private
 
@@ -35,7 +39,5 @@ class PostsController < ApplicationController
 		params.require(:post).permit(*args)
 	  end
 
-	def edit
-	  @post = Post.find(params[:id])
-	end
+	
 end
